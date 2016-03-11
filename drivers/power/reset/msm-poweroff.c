164c3982ceb500e7b1be5c6f52aa9f9ffe915801
Hong-Mei Li <a21834@motorola.com>
2016-03-10 16:57:02 -0800

    power: reset: Set warm restart in panic
    Configure panic as warm restart, so that we have chance to get panic logs.
    
    Change-Id: I28ab926979fac30d44e3d47e8c9491be08734061
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/832045
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -296,6 +296,11 @@ static void msm_restart_prepare(const char *cmd)
 	need_warm_reset = true;
 #endif
 
+#ifdef CONFIG_MACH_LENOVO_KUNTAO
+	if (in_panic)
+		need_warm_reset = true;
+#endif
+
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (need_warm_reset) {
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
