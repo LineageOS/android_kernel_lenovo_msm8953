be63ced5315e8395b2dc66e400da6269bc520d10
Anirudh Ghayal <aghayal@codeaurora.org>
2016-11-24 08:55:04 +0530

    power: smb1351-charger-lenovo: Add mutex locking in parallel enable path
    Add  locking in the parallel charger present callback to avoid
    a race while configuring it from the primary charger.
    
    CRs-Fixed: 1089991
    Change-Id: Ice7f234dcf406f12dadb561d46e049efd9d6ad88
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

--- drivers/power/smb1351-charger-lenovo.c
+++ drivers/power/smb1351-charger-lenovo.c
@@ -572,6 +572,7 @@ struct smb1351_charger {
 	int			slave_fcc_ma_before_esr;
 	int			workaround_flags;
 
+	struct mutex		parallel_config_lock;
 	int			parallel_pin_polarity_setting;
 	bool			is_slave;
 	bool			use_external_fg;
@@ -3283,10 +3284,12 @@ static int smb1351_parallel_set_property(struct power_supply *psy,
 		}
 		break;
 	case POWER_SUPPLY_PROP_PRESENT:
+		mutex_lock(&chip->parallel_config_lock);
 		rc = smb1351_parallel_set_chg_present(chip, val->intval);
 		if (rc)
 			pr_err("Set charger %spresent failed\n",
 					val->intval ? "" : "un-");
+		mutex_unlock(&chip->parallel_config_lock);
 		break;
 	case POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX:
 		if (chip->parallel_charger_present) {
@@ -6207,6 +6210,7 @@ static int smb1351_parallel_slave_probe(struct i2c_client *client,
 				EN_BY_PIN_HIGH_ENABLE : EN_BY_PIN_LOW_ENABLE;
 
 	i2c_set_clientdata(client, chip);
+	mutex_init(&chip->parallel_config_lock);
 
 	chip->parallel_psy.name		= PSY_PARALLEL_NAME;//"usb-parallel";
 	chip->parallel_psy.type		= POWER_SUPPLY_TYPE_USB_PARALLEL;
@@ -6247,6 +6251,7 @@ fail_register_psy:
 	wakeup_source_trash(&chip->smb1351_ws.source);
 	mutex_destroy(&chip->irq_complete);
 	mutex_destroy(&chip->fcc_lock);
+	mutex_destroy(&chip->parallel_config_lock);
 	mutex_destroy(&chip->usb_suspend_status_lock);
 	return rc;
 }
@@ -6275,8 +6280,10 @@ static int smb1351_charger_remove(struct i2c_client *client)
 	mutex_destroy(&chip->irq_complete);
 	mutex_destroy(&chip->fcc_lock);
 	mutex_destroy(&chip->usb_suspend_status_lock);
-	if (is_parallel_slave(client))
+	if (is_parallel_slave(client)) {
+		mutex_destroy(&chip->parallel_config_lock);
 		mutex_destroy(&chip->parallel.lock);
+	}
 	debugfs_remove_recursive(chip->debug_root);
 	return 0;
 }
