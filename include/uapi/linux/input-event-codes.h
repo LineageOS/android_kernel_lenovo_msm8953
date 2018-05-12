c23d68708780e226e14ac6801e50d8015d9f5a35
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-05-12 11:01:46 +0800

    driver: input: misc: Import iqs263 capacity sensor
    * for Lenovo TB8703
    
    Change-Id: I905f8c02dd81df8aa22da485e330ef8fdedf6e5d

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -337,6 +337,10 @@
 
 #define KEY_MICMUTE		248	/* Mute / unmute the microphone */
 
+#define KEY_CAPSENSOR_IQS263_CS0 0x278
+#define KEY_CAPSENSOR_IQS263_CS1 0x279
+#define KEY_CAPSENSOR_IQS263_CS2 0x280
+
 /* Code 255 is reserved for special needs of AT keyboard driver */
 
 #define BTN_MISC		0x100
