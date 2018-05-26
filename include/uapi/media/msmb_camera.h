5a18e4d5b2b56d274884d240577aa824eea3bae8
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-05-26 22:40:12 +0800

    drivers: camera_v2: ifndef ir_cut and ir_led code for nx551j
    Change-Id: I938e8d0b1fda6eba42e22de2a67b9357a2b696d3

--- include/uapi/media/msmb_camera.h
+++ include/uapi/media/msmb_camera.h
@@ -52,9 +52,13 @@
 #define MSM_CAMERA_SUBDEV_SENSOR_INIT  14
 #define MSM_CAMERA_SUBDEV_OIS          15
 #define MSM_CAMERA_SUBDEV_FLASH        16
+#if defined(CONFIG_MACH_LENOVO_TB8703)
+#define MSM_CAMERA_SUBDEV_EXT          17
+#else
 #define MSM_CAMERA_SUBDEV_IR_LED       17
 #define MSM_CAMERA_SUBDEV_IR_CUT       18
 #define MSM_CAMERA_SUBDEV_EXT          19
+#endif
 
 #define MSM_MAX_CAMERA_SENSORS  5
 
