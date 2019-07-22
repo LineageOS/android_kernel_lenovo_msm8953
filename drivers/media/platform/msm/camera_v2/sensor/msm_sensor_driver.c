9e8e3bbebec5fb5f141714fea02a768275cac231
Vitaliy Tomin <highwaystar.ru@gmail.com>
2019-07-22 15:28:42 +0800

    drivers: media: camera: Add tb8704, tb8504, tb8804 changes
    Change-Id: I903edc09259795b407ee35495ce378b5067a5535

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -18,7 +18,7 @@
 #include "msm_cci.h"
 #include "msm_camera_dt_util.h"
 
-#if defined(CONFIG_MACH_LENOVO_TB8703) || defined(CONFIG_MACH_LENOVO_TBX704)
+#if defined(CONFIG_MACH_LENOVO_TB8703) || defined(CONFIG_MACH_LENOVO_TBX704) || defined (CONFIG_MACH_LENOVO_TB8704) ||defined (CONFIG_MACH_LENOVO_TB8804) || defined (CONFIG_MACH_LENOVO_TB8504)
 #include <soc/qcom/camera2.h>
 
 extern struct vendor_eeprom s_vendor_eeprom[CAMERA_VENDOR_EEPROM_COUNT_MAX];
@@ -92,7 +92,7 @@ static int32_t msm_sensor_driver_create_i2c_v4l_subdev
 	struct i2c_client *client = s_ctrl->sensor_i2c_client->client;
 
 	CDBG("%s %s I2c probe succeeded\n", __func__, client->name);
-#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704)
+#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704) && !defined (CONFIG_MACH_LENOVO_TB8704) && !defined (CONFIG_MACH_LENOVO_TB8804) && !defined (CONFIG_MACH_LENOVO_TB8504)
 	if (0 == s_ctrl->bypass_video_node_creation) {
 #endif
 		rc = camera_init_v4l2(&client->dev, &session_id);
@@ -100,7 +100,7 @@ static int32_t msm_sensor_driver_create_i2c_v4l_subdev
 			pr_err("failed: camera_init_i2c_v4l2 rc %d", rc);
 			return rc;
 		}
-#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704)
+#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704) && !defined (CONFIG_MACH_LENOVO_TB8704) && !defined (CONFIG_MACH_LENOVO_TB8804) && !defined (CONFIG_MACH_LENOVO_TB8504)
 	}
 #endif
 
@@ -140,7 +140,7 @@ static int32_t msm_sensor_driver_create_v4l_subdev
 	int32_t rc = 0;
 	uint32_t session_id = 0;
 
-#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704)
+#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704) && !defined (CONFIG_MACH_LENOVO_TB8704) && !defined (CONFIG_MACH_LENOVO_TB8804) && !defined (CONFIG_MACH_LENOVO_TB8504)
 	if (0 == s_ctrl->bypass_video_node_creation) {
 #endif
 		rc = camera_init_v4l2(&s_ctrl->pdev->dev, &session_id);
@@ -148,7 +148,7 @@ static int32_t msm_sensor_driver_create_v4l_subdev
 			pr_err("failed: camera_init_v4l2 rc %d", rc);
 			return rc;
 		}
-#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704)
+#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704) && !defined (CONFIG_MACH_LENOVO_TB8704) && !defined (CONFIG_MACH_LENOVO_TB8804) && !defined (CONFIG_MACH_LENOVO_TB8504)
 	}
 #endif
 
@@ -692,7 +692,7 @@ static void msm_sensor_fill_sensor_info(struct msm_sensor_ctrl_t *s_ctrl,
 	strlcpy(entity_name, s_ctrl->msm_sd.sd.entity.name, MAX_SENSOR_NAME);
 }
 
-#if defined(CONFIG_MACH_LENOVO_TB8703) || defined(CONFIG_MACH_LENOVO_TBX704)
+#if defined(CONFIG_MACH_LENOVO_TB8703) || defined(CONFIG_MACH_LENOVO_TBX704) || defined(CONFIG_MACH_LENOVO_TB8704) || defined(CONFIG_MACH_LENOVO_TB8804) || defined(CONFIG_MACH_LENOVO_TB8504)
 /* add sensor info for factory mode
    begin
 */
@@ -768,10 +768,9 @@ int32_t msm_sensor_driver_probe(void *setting,
 	struct msm_camera_cci_client        *cci_client = NULL;
 	struct msm_camera_sensor_slave_info *slave_info = NULL;
 	struct msm_camera_slave_info        *camera_info = NULL;
-#if defined(CONFIG_MACH_LENOVO_TB8703) || defined(CONFIG_MACH_LENOVO_TBX704)
+#if defined(CONFIG_MACH_LENOVO_TB8703) || defined(CONFIG_MACH_LENOVO_TBX704) || defined (CONFIG_MACH_LENOVO_TB8704) ||defined (CONFIG_MACH_LENOVO_TB8804) || defined (CONFIG_MACH_LENOVO_TB8504)
 	int32_t i = 0;
 #endif
-
 	unsigned long                        mount_pos = 0;
 	uint32_t                             is_yuv;
 
@@ -844,7 +843,7 @@ int32_t msm_sensor_driver_probe(void *setting,
 			slave_info32->sensor_init_params;
 		slave_info->output_format =
 			slave_info32->output_format;
-#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704)
+#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704) && !defined (CONFIG_MACH_LENOVO_TB8704) && !defined (CONFIG_MACH_LENOVO_TB8804) && !defined (CONFIG_MACH_LENOVO_TB8504)
 		slave_info->bypass_video_node_creation =
 			!!slave_info32->bypass_video_node_creation;
 #endif
@@ -861,6 +860,55 @@ int32_t msm_sensor_driver_probe(void *setting,
 			goto free_slave_info;
 		}
 	}
+#if defined (CONFIG_MACH_LENOVO_TB8504)
+	//lct.hzy added for eeprom match id 20161219
+	if(s_vendor_eeprom[i].eeprom_name != NULL){
+	    for(i=0; i<CAMERA_VENDOR_EEPROM_COUNT_MAX; i++){
+	        if(strcmp(slave_info->eeprom_name,s_vendor_eeprom[i].eeprom_name) == 0){
+                    //s_vendor_eeprom is from kernel camera dtsi
+	            CDBG(" dtsi eeprom_name[%d]=%s, module_id=%d\n",i,s_vendor_eeprom[i].eeprom_name, s_vendor_eeprom[i].module_id);
+	            if(((strcmp(slave_info->sensor_name,"qtech_ov5695") == 0) && (s_vendor_eeprom[i].module_id == MID_QTECH))
+		        || ((strcmp(slave_info->sensor_name,"sunny_ov5695") == 0) && (s_vendor_eeprom[i].module_id == MID_SUNNY))
+		        || ((strcmp(slave_info->sensor_name,"sp2509") == 0))
+		        || ((strcmp(slave_info->sensor_name,"hi259") == 0))
+		    ){
+		        CDBG("module found!probe continue!\n");
+		        break;
+		     }
+	        }
+	    }
+	    if(i >= CAMERA_VENDOR_EEPROM_COUNT_MAX){
+		pr_err("module not found!probe break!\n");
+		rc = -EFAULT;
+		goto free_slave_info;
+	    }
+	}
+#endif/*CONFIG_MACH_LENOVO_TB8504*/
+#if defined (CONFIG_MACH_LENOVO_TB8704) ||defined (CONFIG_MACH_LENOVO_TB8804)
+	//lct.CHB added for eeprom match id 20161101
+	if(s_vendor_eeprom[i].eeprom_name != NULL){
+	    for(i=0; i<CAMERA_VENDOR_EEPROM_COUNT_MAX; i++){
+	        if(strcmp(slave_info->eeprom_name,s_vendor_eeprom[i].eeprom_name) == 0){
+                    //s_vendor_eeprom is from kernel camera dtsi
+	            CDBG(" dtsi eeprom_name[%d]=%s, module_id=%d\n",i,s_vendor_eeprom[i].eeprom_name, s_vendor_eeprom[i].module_id);
+	            if(((strcmp(slave_info->sensor_name,"ov5695_f5695ak") == 0) && (s_vendor_eeprom[i].module_id == MID_QTECH))
+		        || ((strcmp(slave_info->sensor_name,"ov5695_ccbfl05006") == 0) && (s_vendor_eeprom[i].module_id == MID_LITEARRAY))
+		        || ((strcmp(slave_info->sensor_name,"imx219_fx219bh") == 0) && (s_vendor_eeprom[i].module_id == MID_QTECH))
+		        || ((strcmp(slave_info->sensor_name,"imx219_l219a00") == 0) && (s_vendor_eeprom[i].module_id == MID_OFILM))
+		        || ((strcmp(slave_info->sensor_name,"ov8856") == 0))
+		    ){
+		            CDBG("module found!probe continue!\n");
+		            break;
+		     }
+	        }
+	    }
+	    if(i >= CAMERA_VENDOR_EEPROM_COUNT_MAX){
+		pr_err("module not found!probe break!\n");
+		rc = -EFAULT;
+		goto free_slave_info;
+	    }
+	}
+#endif/*CONFIG_MACH_LENOVO_TB8704*/
 
 	if (strlen(slave_info->sensor_name) >= MAX_SENSOR_NAME ||
 		strlen(slave_info->eeprom_name) >= MAX_SENSOR_NAME ||
@@ -926,7 +974,7 @@ int32_t msm_sensor_driver_probe(void *setting,
 		slave_info->sensor_init_params.position);
 	CDBG("mount %d",
 		slave_info->sensor_init_params.sensor_mount_angle);
-#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704)
+#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704) && !defined (CONFIG_MACH_LENOVO_TB8704) && !defined (CONFIG_MACH_LENOVO_TB8804) && !defined (CONFIG_MACH_LENOVO_TB8504)
 	CDBG("bypass video node creation %d",
 		slave_info->bypass_video_node_creation);
 #endif
@@ -1096,7 +1144,7 @@ CSID_TG:
 
 	pr_err("%s probe succeeded", slave_info->sensor_name);
 
-#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704)
+#if !defined(CONFIG_MACH_LENOVO_TB8703) && !defined(CONFIG_MACH_LENOVO_TBX704) && !defined (CONFIG_MACH_LENOVO_TB8704) && !defined (CONFIG_MACH_LENOVO_TB8804) && !defined (CONFIG_MACH_LENOVO_TB8504)
 	s_ctrl->bypass_video_node_creation =
 		slave_info->bypass_video_node_creation;
 #endif
@@ -1153,7 +1201,7 @@ CSID_TG:
 
 	msm_sensor_fill_sensor_info(s_ctrl, probed_info, entity_name);
 
-#if defined(CONFIG_MACH_LENOVO_TB8703) || defined(CONFIG_MACH_LENOVO_TBX704)
+#if defined(CONFIG_MACH_LENOVO_TB8703) || defined(CONFIG_MACH_LENOVO_TBX704) || defined (CONFIG_MACH_LENOVO_TB8704) ||defined (CONFIG_MACH_LENOVO_TB8804) || defined (CONFIG_MACH_LENOVO_TB8504)
 	msm_sensor_init_device_name();
 	msm_sensor_set_module_info(s_ctrl);
 #endif
@@ -1163,6 +1211,8 @@ CSID_TG:
 	 * probed on this slot
 	 */
 	s_ctrl->is_probe_succeed = 1;
+	msm_sensor_init_device_name();
+	msm_sensor_set_module_info(s_ctrl);
 	return rc;
 
 camera_power_down:
